0a2e04f2bb7c5a9851a08326f23419dc1b945df0
ahmedseliya <ahmedseliya07@gmail.com>
2025-08-06 22:56:41 +0530

    hey

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,11 +3,11 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBKnUIMEh4w-drmwNEx2nT_pRMLjR9m_gA",
+  apiKey: "AIzaSyBKnUIMEh4w-drmwNEx2nT_pRMLjR9m_gA",
   authDomain: "cybershield-40ca4.firebaseapp.com",
   databaseURL: "https://cybershield-40ca4-default-rtdb.firebaseio.com",
   projectId: "cybershield-40ca4",
-  storageBucket: "cybershield-40ca4.firebasestorage.app",
+  storageBucket: "cybershield-40ca4.appspot.com", // ✅ Corrected this line
   messagingSenderId: "572424733926",
   appId: "1:572424733926:web:107a91db893fb5d9e8a727"
 };
